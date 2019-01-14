f17618bd17f7f7c59325068c7a7a8e3007c27552
Shiro <tailt9@fpt.com.vn>
2019-01-14 16:38:30 +0700

    VSE_002 feat: add api show list device by email

--- app/Http/Controllers/DoorAlarmController.php
+++ app/Http/Controllers/DoorAlarmController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Models\Customer;
 use App\Http\Models\DeviceToken;
-use App\Http\Requests\DoorAlarm as DoorAlarmRequest;
+use App\Http\Requests\Customer as CustomerRequest;
 use App\Http\Models\DoorAlarm;
 use Illuminate\Http\Request;
 
@@ -22,9 +22,69 @@ class DoorAlarmController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
 
+        $email = $request->email;
+        $requestCus = new CustomerRequest();
+        $validator = $requestCus->checkValidate($request);
+        if ($validator->fails())
+            return $this->responseFormat(422, $validator->errors());
+//        $cusId = Customer::where(['email' => $email, 'is_deleted' => 0])->pluck('id')->first();
+//        if (!$cusId)
+//            return $this->responseFormat(404, 'Not found customer');
+//        $doorAlarmId = DeviceToken::where(['customer_id' => $cusId])->pluck('dooralarm_id');
+//        if (!$doorAlarmId)
+//            return $this->responseFormat(404, 'Not found door alarm');
+//        $doorAlarm = DoorAlarm::find($doorAlarmId);
+//        if (!$doorAlarm)
+//            return $this->responseFormat(404, 'Empty');
+        $doorAlarmData = [
+            "id" => 1,
+            "email" => "huy1@gmail.com",
+            "nick_name" => "huy",
+            "created_at" => "2019-01-04 09:35:44",
+            "updated_at" => "2019-01-04 09:35:44",
+            "devices" => [
+                [
+                    "id" => 1,
+                    "MAC" => "DC:4f:22:BA:BA:7B",
+                    "name" => "huy1",
+                    "password" => "123123",
+                    "location" => "123123sdas",
+                    "version" => "1.0",
+                    "volume" => "2",
+                    "arm_delay" => "5s",
+                    "alarm_delay" => "5s",
+                    "alarm_duration" => "5s",
+                    "self_test_mode" => "normal",
+                    "is_arm" => 1,
+                    'is_home' => 1,
+                    'is_alarm' => 0,
+                    'door_status' => 1,
+                    "battery_capacity_reamaining" => "50%",
+                ],
+                [
+                    "id" => 2,
+                    "MAC" => "DC:4f:22:BA:BA:7B",
+                    "name" => "huy2",
+                    "password" => "123123",
+                    "location" => "123123sdas",
+                    "version" => "1.0",
+                    "volume" => "2",
+                    "arm_delay" => "5s",
+                    "alarm_delay" => "5s",
+                    "alarm_duration" => "5s",
+                    "self_test_mode" => "normal",
+                    "is_arm" => 1,
+                    'is_home' => 1,
+                    'is_alarm' => 0,
+                    'door_status' => 1,
+                    "battery_capacity_reamaining" => "50%",
+                ]
+            ]
+        ];
+        return $this->responseFormat(200, 'Success', $doorAlarmData);
     }
 
     /**
@@ -44,8 +104,7 @@ class DoorAlarmController extends Controller
 //            return $this->responseFormat(404, 'Not found');
         $doorAlarm = new DoorAlarm();
         $doorAlarm->fill($request->all());
-        $deviceToken = new \App\Repositories\DeviceToken();
-        return $deviceToken->create($request);
+
     }
 
     /**
---
+++ app/Http/Requests/Customer.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class Customer extends FormRequest
+{
+    private $ins = true;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        if ($this->ins)
+            return [
+                'email' => 'required|email',
+            ];
+        else
+            return [
+
+            ];
+    }
+
+    public function checkValidate(Request $request, $ins = true)
+    {
+        if (!$ins)
+            $this->ins = false;
+        $validator = Validator::make($request->all(), $this->rules());
+        return $validator;
+    }
+}
